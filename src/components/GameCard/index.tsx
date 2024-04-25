4d40c9f99fd36e0e430339c8c33db630b9def2e3
batalovmv <55100840+batalovmv@users.noreply.github.com>
2024-04-26 00:57:52 +0500

    добавил модалку для открытия скриншота

--- src/components/GameCard/index.tsx
+++ src/components/GameCard/index.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import './style.css';
-import { Carousel } from 'antd';
+import { Card, Carousel, Modal } from 'antd';
 
 interface Game {
     id: number;
@@ -23,9 +23,14 @@ interface GameCardProps {
 const GameCard: React.FC<GameCardProps> = ({ game, getScreens }) => {
     const [isHovering, setIsHovering] = useState(false);
     const [screenshotsLoaded, setScreenshotsLoaded] = useState(false);
-    const handleMouseEnter =  () => {
+    const [loading, setLoading] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [modalImage, setModalImage] = useState('');
+    const handleMouseEnter = async () => {
         if (!screenshotsLoaded) {
-            getScreens(game.id)
+            setLoading(true);
+            await getScreens(game.id)
+            setLoading(false);
             setScreenshotsLoaded(true);
         }
         setIsHovering(true);
@@ -34,26 +39,49 @@ const GameCard: React.FC<GameCardProps> = ({ game, getScreens }) => {
     const handleMouseLeave = () => {
         setIsHovering(false);
     };
+    const showModal = (image:string) => {
+        setModalImage(image);
+        setIsModalOpen(true);
+    };
+
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
      return (
-        <div className="game-card" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+         <Card className="game-card" loading={loading} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+       
             <h3>{game.title}</h3>
             {isHovering && game.screenshots?.length ? (
+                <>
                 <Carousel autoplay>
                     {game.screenshots.map((screenshot, index) => (
-                        <div key={index}>
-                            <img src={screenshot} alt={`Screenshot ${index + 1}`} style={{ width: '100%' }} />
+                        <div key={index} onClick={() => showModal(screenshot)}>
+                            <img src={screenshot} alt={`Screenshot ${index + 1}`} />
                         </div>
                     ))}
+                    
                 </Carousel>
+                <Modal  
+        open={isModalOpen} 
+        onCancel={closeModal} 
+        footer={null}
+                         width={'80%'}
+        centered
+      >
+        <img src={modalImage} alt="Expanded screenshot"  style={{width:'100%'}}  />
+          </Modal >
+                 </>
+                
             ) : (
-                <img src={game.coverImage} alt={game.title} style={{ width: '100%' }} />
+                     <img src={game.coverImage} alt={game.title} style={{ width: '100%', maxHeight: '80vh' }} />
             )}
             <p>Rating: {game.rating}</p>
             <p>Platforms: {game.platforms.join(', ')}</p>
             {game.multiplayerInfo.online && (
                 <p>Online Multiplayer: Up to {game.multiplayerInfo.maxPlayers} players</p>
             )}
-        </div>
+        
+         </Card>
     );
 };
 
