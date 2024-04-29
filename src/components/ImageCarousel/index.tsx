1a7dc61f1298c6cb57259b05ca6c2a87b92d067f
batalovmv <55100840+batalovmv@users.noreply.github.com>
2024-04-30 01:52:59 +0500

    исправлены проблемы с Image.PreviewGroup

--- src/components/ImageCarousel/index.tsx
+++ src/components/ImageCarousel/index.tsx
@@ -8,40 +8,37 @@ interface ImageCarouselProps {
 }
 const ImageCarousel = ({ screens, game, isHoveringImage  }: ImageCarouselProps) => {
     const [loadedImagesCount, setLoadedImagesCount] = useState(0);
-
+    const [currentIndex, setСurrentIndex] = useState(0)
     const handleImageLoad = () => {
         setLoadedImagesCount(prevCount => prevCount + 1);
+        console.log(`screens`, screens);
     };
     console.log(`screens`, screens);
-     if (isHoveringImage && screens.length>0) {
+    if (isHoveringImage && screens.length > 0) {
         return <>
-            <Image.PreviewGroup preview={{
-                onChange: (current, prev) => console.log(`current index: ${current}, prev index: ${prev}`),
-            }}>
-                <Carousel autoplay draggable dots={true} style={{ display: loadedImagesCount >= screens.length ? 'block' : 'none' }}>
-                    {screens.map((screen, index) => (
-                        <Image
-                            key={index}
-                            src={screen.thumb}
-                            width={'100%'}
-                            alt={`Screenshot ${index + 1}`}
-                            onLoad={() => handleImageLoad()}
-                            preview={{
-                                src: screen.full,
-                            }}
-
-                        />
-                    ))}
-                </Carousel>
-                <Spin style={{ display: loadedImagesCount < screens.length ? 'block' : 'none' }} >
-                    <Image style={{ display: loadedImagesCount < screens.length ? 'block' : 'none' }} src={game.coverImage} />
-
-
-                </Spin>
-                
-            </Image.PreviewGroup>
+            <Spin spinning={loadedImagesCount < screens.length}>
+                <Image.PreviewGroup preview={{ 
+                     current: currentIndex,
+                     onChange: (current: number) => { setСurrentIndex(current) },
+                       }} items={screens.map(e=>e.full)}>
+                    <Carousel  dots={true} beforeChange={(_, next) => setСurrentIndex(next)} style={{ display: loadedImagesCount >= screens.length ? 'block' : 'none' }}>
+                        {screens.map((screen, index) => (
+                            <Image
+                                key={screen.full}
+                                src={screen.thumb}
+                                width={'100%'}
+                                alt={`Screenshot ${index + 1}`}
+                                onLoad={handleImageLoad}
+                             
+                            />
+                        ))}
+                  
+                    </Carousel>
+                </Image.PreviewGroup>
+            </Spin>
+            <Image src={game.coverImage} style={{ display: 'none' }} />
         </>
-     } else if (isHoveringImage){
+    } else if (isHoveringImage) {
          return <Spin  >
              <Image  src={game.coverImage}
                  placeholder={
